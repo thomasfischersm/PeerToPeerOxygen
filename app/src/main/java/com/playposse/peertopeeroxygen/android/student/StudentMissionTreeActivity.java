b0129ceb1a65d01cd5af0f3c76e4d81b1ca9b6a6
thomasfischersm <thomas@playposse.com>
2016-12-18 20:58:35 -0800

    Made student mission tree activity have one dot for every level.

--- app/src/main/java/com/playposse/peertopeeroxygen/android/student/StudentMissionTreeActivity.java
+++ app/src/main/java/com/playposse/peertopeeroxygen/android/student/StudentMissionTreeActivity.java
@@ -2,6 +2,11 @@ package com.playposse.peertopeeroxygen.android.student;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -9,7 +14,10 @@ import com.playposse.peertopeeroxygen.android.R;
 import com.playposse.peertopeeroxygen.android.data.DataRepository;
 import com.playposse.peertopeeroxygen.android.model.ExtraConstants;
 import com.playposse.peertopeeroxygen.android.ui.widgets.MissionTreeWidget;
+import com.playposse.peertopeeroxygen.backend.peerToPeerOxygenApi.model.MissionLadderBean;
 import com.playposse.peertopeeroxygen.backend.peerToPeerOxygenApi.model.MissionTreeBean;
+
+import java.util.List;
 
 /**
  * Activity that shows the student the {@link MissionTreeBean}.
@@ -17,12 +25,8 @@ import com.playposse.peertopeeroxygen.backend.peerToPeerOxygenApi.model.MissionT
 public class StudentMissionTreeActivity extends StudentParentActivity {
 
     private Long missionLadderId;
-    private Long missionTreeId;
-    private MissionTreeBean missionTreeBean;
 
-    private MissionTreeWidget missionTreeWidget;
-    private ImageButton levelDownImageButton;
-    private ImageButton levelUpImageButton;
+    private ViewPager missionTreePager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,48 +35,95 @@ public class StudentMissionTreeActivity extends StudentParentActivity {
 
         Intent intent = getIntent();
         missionLadderId = intent.getLongExtra(ExtraConstants.EXTRA_MISSION_LADDER_ID, -1);
-        missionTreeId = intent.getLongExtra(ExtraConstants.EXTRA_MISSION_TREE_ID, -1);
 
-        missionTreeWidget = (MissionTreeWidget) findViewById(R.id.missionTreeWidget);
-        levelDownImageButton = (ImageButton) findViewById(R.id.levelDownImageButton);
-        levelUpImageButton = (ImageButton) findViewById(R.id.levelUpImageButton);
+        missionTreePager = (ViewPager) findViewById(R.id.missionTreePager);
     }
 
     @Override
-    public void receiveData(DataRepository dataRepository) {
-        missionTreeBean = dataRepository
-                .getMissionTreeBean(missionLadderId, missionTreeId);
-        missionTreeWidget.setMissionTreeBean(
-                missionLadderId,
-                missionTreeBean,
-                dataRepository);
-        String title = String.format(
-                getString(R.string.student_mission_tree_activity_title),
-                missionTreeBean.getLevel(),
-                missionTreeBean.getName());
-        setTitle(title);
-
-        initLevelButton(dataRepository, levelDownImageButton, missionTreeBean.getLevel() - 1);
-        initLevelButton(dataRepository, levelUpImageButton, missionTreeBean.getLevel() + 1);
-    }
-
-    private void initLevelButton(DataRepository dataRepository, ImageButton button, int level) {
-        final MissionTreeBean otherMissionTreeBean = dataRepository.getMissionTreeBeanByLevel(
-                missionLadderId,
-                level);
-        button.setVisibility(otherMissionTreeBean != null ? View.VISIBLE : View.GONE);
-        button.setOnClickListener(new View.OnClickListener() {
+    public void receiveData(final DataRepository dataRepository) {
+        missionTreePager.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                finish();
-                Intent intent = ExtraConstants.createIntent(
-                        getApplicationContext(),
-                        StudentMissionTreeActivity.class,
-                        missionLadderId,
-                        otherMissionTreeBean.getId(),
-                        null);
-                startActivity(intent);
+            public void run() {
+                if (missionTreePager.getHandler() != null) {
+                    MissionLadderBean missionLadderBean =
+                            dataRepository.getMissionLadderBean(missionLadderId);
+                    MissionTreePagerAdapter pagerAdapter = new MissionTreePagerAdapter(
+                            getSupportFragmentManager(),
+                            missionLadderBean);
+                    missionTreePager.setAdapter(pagerAdapter);
+                    missionTreePager.addOnPageChangeListener(pagerAdapter);
+                }
             }
         });
+    }
+
+    /**
+     * A {@link android.support.v4.view.PagerAdapter} that shows a fragment for each level.
+     */
+    private final class MissionTreePagerAdapter
+            extends FragmentStatePagerAdapter implements
+            ViewPager.OnPageChangeListener {
+
+        private final MissionLadderBean missionLadderBean;
+
+        private MissionTreePagerAdapter(
+                FragmentManager fragmentManager,
+                MissionLadderBean missionLadderBean) {
+
+            super(fragmentManager);
+
+            this.missionLadderBean = missionLadderBean;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MissionTreeBean missionTreeBean = getMissonTreeBeanByPosition(position);
+            if (missionTreeBean != null) {
+                return StudentMissionTreeFragment.newInstance(
+                        missionLadderBean.getId(),
+                        missionTreeBean.getId());
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            List<MissionTreeBean> missionTreeBeans = missionLadderBean.getMissionTreeBeans();
+            return (missionTreeBeans != null) ? missionTreeBeans.size() : 0;
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            // Ignore.
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            MissionTreeBean missionTreeBean = getMissonTreeBeanByPosition(position);
+            if (missionTreeBean != null) {
+                String title = String.format(
+                        getString(R.string.student_mission_tree_activity_title),
+                        missionTreeBean.getLevel(),
+                        missionTreeBean.getName());
+                setTitle(title);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            // Ignore.
+        }
+
+        @Nullable
+        private MissionTreeBean getMissonTreeBeanByPosition(int position) {
+            List<MissionTreeBean> missionTreeBeans = missionLadderBean.getMissionTreeBeans();
+            if ((missionTreeBeans != null) && (position < missionTreeBeans.size())) {
+
+                return missionTreeBeans.get(position);
+            }
+
+            return null;
+        }
     }
 }
